9d189b1a8ea6dcf65e6fb8e6612d414c4ca2ca0f
lzy <1296755703@qq.com>
2021-11-21 15:53:54 +0800

    基本数据类型

--- day2/javabase/Variable2.java
+++ day2/javabase/Variable2.java
@@ -29,7 +29,7 @@ package day2.javabase;
 */
 public class Variable2 {
     public static void main(String[] args) {
-        // 定义变量  先  在 
+        // 定义变量  先 在  
         int myAge = 12;
         System.out.println(myAge);
         // 也可以先定义
@@ -38,22 +38,68 @@ public class Variable2 {
         newYear = 2021;
         System.out.println(newYear);
 
-        // 整型 byte(1字节=8bit) ;short(2字节);int(4字节) ;long(8字节);
+        // 1 整型 byte(1字节=8bit) ;short(2字节);int(4字节) ;long(8字节);
         // byte范围-128~127
         byte b1 = 12;
         byte b2 = -128;
-        // b2 = 128; 超出byte范围 编译补通过
+        // b2 = 128; 超出byte范围 编译不通过
         System.out.println(b1);
         System.out.println(b2);
 
         /*
-        声明long型变量，必须以l 或 L 结尾巴
-        通常，定义整型变量时，使用int
-        */ 
+         * 声明long型变量，必须以l 或 L 结尾 通常，定义整型变量时，使用int
+         */
         short s1 = 128;
         int s2 = 1234;
         long s3 = 324234234L;
         System.out.println(s3);
+
+        /*
+         * 2 浮点型 (浮点型表示带小数点的数值)  float (4字节) doubule(8字节) float
+         * 表示数值的范围比long还大
+         * 
+         */
+
+         // 定义float类型的变量时，变量要以f或F结尾
+        float d1 = 12.3f;
+        System.out.println(d1);
+        /*
+        通常我们定义浮点型变脸的时候 使用double范围更大 精度更高
+        */
+        double d2 = 123.33;
+        System.out.println(d2);
+
+        /*
+        3  字符型：char（1字符=2字节）
+        定义char型变量 通常使用一堆''
+        */
+        char c1='a';
+        char c2='喊';
+        // c1='AB';  字符型 只可以赋值一个字符 或汉字 或其他类型的文字
+        System.out.println(c1);
+        System.out.println(c2);
+
+        //表示方式 1： 声明 一个字符 2 转移字符   3 直接使用Unicode 值来表示字符型常量
+        char c3='\n';//转义字符
+        c3='\t';//制表符
+        char c4='\u0043';
+        System.out.print("hello"+c3);
+        System.out.println("world");
+        System.out.println(c4);
+
+
+        /*
+        4  布尔型：boolean(常在循环中 或条件判断中使用)
+        之能取两个值  true ,false
+        */
+        boolean bb1=true;
+        System.out.println(bb1);
+        boolean isMarried=true;
+        if(isMarried){
+            System.out.println("不能参加单身\"单身\"party了\\n 很遗憾");
+        }else{
+            System.out.println("快来找女朋友呀!");
+        }
     }
 
 }
