035625f33739e288932eafa7d614bbcfa7f41776
lizhiyi <12967557033@qq.com>
2021-11-22 13:57:55 +0800

    1

--- day2/javabase/VariableTest6.java
+++ day2/javabase/VariableTest6.java
@@ -17,9 +17,9 @@ public class VariableTest6 {
         int num3 = 0127;
         int num4 = 0x110A;
 
-        System.out.println("num1= :" + num1);
-        System.out.println("num2= :" + num2);
-        System.out.println("num3= :" + num3);
-        System.out.println("num4= :" + num4);
+        System.out.println("num1= :" + num1);//6
+        System.out.println("num2= :" + num2);//110
+        System.out.println("num3= :" + num3);//87
+        System.out.println("num4= :" + num4);//4362
     }
 }
